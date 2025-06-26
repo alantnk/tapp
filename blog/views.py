77f638fb589e28ae8e9af4339ea525758ac4f462
tnka <tnka@xyz.com>
2025-06-26 11:54:17 -0300

    feat: add custom template tags

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from functools import partial
 
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Count
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import View
@@ -52,13 +53,19 @@ class PostDetailView(View):
         comments = post.comments.filter(active=True)
         # Form for users to comment
         form = CommentForm()
+        post_tags_ids = post.tags.values_list('id', flat=True)
+        similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+        similar_posts = similar_posts.annotate(
+            same_tags=Count('tags')
+        ).order_by('-same_tags', '-publish')[:4]
         return render(
             request,
             'blog/post/detail.html',
             {
                 'post': post,
                 'comments': comments,
-                'form': form
+                'form': form,
+                'similar_posts': similar_posts,
             },
         )
 
